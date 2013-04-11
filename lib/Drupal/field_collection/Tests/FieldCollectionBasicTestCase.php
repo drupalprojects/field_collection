66741d435a336cf2c69d60f19dc8133de6e638d2
Lee Rowlands <lee.rowlands@previousnext.com.au>
2013-04-11 17:34:07 +1000

    Issue #1443960 by larowlan: Converted tests to PSR-0

--- lib/Drupal/field_collection/Tests/FieldCollectionBasicTestCase.php
+++ lib/Drupal/field_collection/Tests/FieldCollectionBasicTestCase.php
@@ -2,13 +2,38 @@
 
 /**
  * @file
- * field_collections tests.
+ * Contains \Drupal\field_collection\Tests\FieldCollectionBasicTestCase.
  */
+
+use Drupal\simpletest\WebTestBase;
+
+namespace Drupal\field_collection\Tests;
 
 /**
  * Test basics.
  */
-class FieldCollectionBasicTestCase extends DrupalWebTestCase {
+class FieldCollectionBasicTestCase extends WebTestBase {
+
+  /**
+   * Field collection field.
+   *
+   * @var
+   */
+  protected $field;
+
+  /**
+   * Field collection field instance.
+   *
+   * @var
+   */
+  protected $instance;
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = array('field_collection');
 
   public static function getInfo() {
     return array(
@@ -18,12 +43,16 @@ class FieldCollectionBasicTestCase extends DrupalWebTestCase {
     );
   }
 
-  function setUp() {
-    parent::setUp('field_collection');
+  public function setUp() {
+    parent::setUp();
 
     // Create a field_collection field to use for the tests.
     $this->field_name = 'field_test_collection';
-    $this->field = array('field_name' => $this->field_name, 'type' => 'field_collection', 'cardinality' => 4);
+    $this->field = array(
+      'field_name' => $this->field_name,
+      'type' => 'field_collection',
+      'cardinality' => 4
+    );
     $this->field = field_create_field($this->field);
     $this->field_id = $this->field['id'];
 
@@ -60,7 +89,7 @@ class FieldCollectionBasicTestCase extends DrupalWebTestCase {
   /**
    * Tests CRUD.
    */
-  function testCRUD() {
+  public function testCRUD() {
     list ($node, $entity) = $this->createNodeWithFieldCollection();
     $node = node_load($node->nid, NULL, TRUE);
     $this->assertEqual($entity->item_id, $node->{$this->field_name}[LANGUAGE_NONE][0]['value'], 'A field_collection has been successfully created and referenced.');
@@ -215,7 +244,7 @@ class FieldCollectionBasicTestCase extends DrupalWebTestCase {
   /**
    * Make sure the basic UI and access checks are working.
    */
-  function testBasicUI() {
+  public function testBasicUI() {
     // Add a field to the collection.
     $field = array(
       'field_name' => 'field_text',
@@ -298,125 +327,5 @@ class FieldCollectionBasicTestCase extends DrupalWebTestCase {
     $this->assertNoLinkByHref('field-collection/field-test-collection/2/delete', 'No links on revision view.');
 
     $this->drupalGet("node/$node->nid/revisions");
-  }
-}
-
-
-/**
- * Test using field collection with Rules.
- */
-class FieldCollectionRulesIntegrationTestCase extends DrupalWebTestCase {
-
-  public static function getInfo() {
-    return array(
-      'name' => 'Field collection Rules integration',
-      'description' => 'Tests using field collections with rules.',
-      'group' => 'Field types',
-      'dependencies' => array('rules'),
-    );
-  }
-
-  function setUp() {
-    parent::setUp(array('field_collection', 'rules'));
-    variable_set('rules_debug_log', 1);
-  }
-
-  protected function createFields($cardinality = 4) {
-    // Create a field_collection field to use for the tests.
-    $this->field_name = 'field_test_collection';
-    $this->field = array('field_name' => $this->field_name, 'type' => 'field_collection', 'cardinality' => $cardinality);
-    $this->field = field_create_field($this->field);
-    $this->field_id = $this->field['id'];
-
-    $this->instance = array(
-      'field_name' => $this->field_name,
-      'entity_type' => 'node',
-      'bundle' => 'article',
-      'label' => $this->randomName() . '_label',
-      'description' => $this->randomName() . '_description',
-      'weight' => mt_rand(0, 127),
-      'settings' => array(),
-      'widget' => array(
-        'type' => 'hidden',
-        'label' => 'Test',
-        'settings' => array(),
-      ),
-    );
-    $this->instance = field_create_instance($this->instance);
-    // Add a field to the collection.
-    $field = array(
-      'field_name' => 'field_text',
-      'type' => 'text',
-      'cardinality' => 1,
-      'translatable' => FALSE,
-    );
-    field_create_field($field);
-    $instance = array(
-      'entity_type' => 'field_collection_item',
-      'field_name' => 'field_text',
-      'bundle' => $this->field_name,
-      'label' => 'Test text field',
-      'widget' => array(
-        'type' => 'text_textfield',
-      ),
-    );
-    field_create_instance($instance);
-  }
-
-  /**
-   * Test creation field collection items.
-   */
-  function testCreation() {
-    $this->createFields();
-
-    $node = $this->drupalCreateNode(array('type' => 'article'));
-    // Create a field collection.
-    $action_set = rules_action_set(array('node' => array('type' => 'node', 'bundle' => 'article')));
-    $action_set->action('entity_create', array(
-      'type' => 'field_collection_item',
-      'param_field_name' => $this->field_name,
-      'param_host_entity:select' => 'node',
-    ));
-    $action_set->action('data_set', array('data:select' => 'entity-created:field-text', 'value' => 'foo'));
-    $action_set->execute($node);
-
-    $node = node_load($node->nid, NULL, TRUE);
-    $this->assertTrue(!empty($node->{$this->field_name}[LANGUAGE_NONE][0]['value']), 'A field_collection has been successfully created.');
-    $this->assertTrue(!empty($node->{$this->field_name}[LANGUAGE_NONE][0]['revision_id']), 'A field_collection has been successfully created (revision).');
-
-    // Now try making use of the field collection in rules.
-    $action_set = rules_action_set(array('node' => array('type' => 'node', 'bundle' => 'article')));
-    $action_set->action('drupal_message', array('message:select' => 'node:field-test-collection:0:field-text'));
-    $action_set->execute($node);
-
-    $msg = drupal_get_messages();
-    $this->assertEqual(array_pop($msg['status']), 'foo', 'Field collection can be used.');
-    RulesLog::logger()->checkLog();
-  }
-
-  /**
-   * Test using field collection items via the host while they are being created.
-   */
-  function testUsageDuringCreation() {
-    // Test using a single-cardinality field collection.
-    $this->createFields(1);
-
-    $node = $this->drupalCreateNode(array('type' => 'article'));
-    $entity = entity_create('field_collection_item', array('field_name' => $this->field_name));
-    $entity->setHostEntity('node', $node);
-    // Now the field collection is linked to the host, but not yet saved.
-
-    // Test using the wrapper on it.
-    $wrapper = entity_metadata_wrapper('node', $node);
-    $wrapper->get($this->field_name)->field_text->set('foo');
-    $this->assertEqual($entity->field_text[LANGUAGE_NONE][0]['value'], 'foo', 'Field collection item used during creation via the wrapper.');
-
-    // Now test it via Rules, which should save our changes.
-    $set = rules_action_set(array('node' => array('type' => 'node', 'bundle' => 'article')));
-    $set->action('data_set', array('data:select' => 'node:' . $this->field_name . ':field-text', 'value' => 'bar'));
-    $set->execute($node);
-    $this->assertEqual($entity->field_text[LANGUAGE_NONE][0]['value'], 'bar', 'Field collection item used during creation via Rules.');
-    $this->assertTrue(!empty($entity->item_id) && !empty($entity->revision_id), 'Field collection item has been saved by Rules and the host entity.');
-    RulesLog::logger()->checkLog();
   }
 }
