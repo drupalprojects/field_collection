3d1398b42794f0fa580000895d88a8db9a242b6f
jmuzz <joel.muzzerall@gmail.com>
2013-12-19 12:48:39 -0400

    Issue #1443960 by jmuzz: Replaced hook_field_info with a FieldType plugin and ifed out the original Entity class as it causes a fatal error now

---
+++ lib/Drupal/field_collection/Plugin/Field/FieldType/FieldCollection.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\field_collection\Plugin\Field\FieldType\FieldCollectionItem.
+ */
+
+namespace Drupal\field_collection\Plugin\Field\FieldType;
+
+use Drupal\Core\Field\ConfigFieldItemBase;
+use Drupal\field\FieldInterface;
+use Drupal\Core\TypedData\DataDefinition;
+
+/**
+ * Plugin implementation of the 'field_collection' field type.
+ *
+ * @FieldType(
+ *   id = "field_collection",
+ *   label = @Translation("Field collection"),
+ *   description = @Translation("This field stores references to embedded entities, which itself may contain any number of fields."),
+ *   settings = {
+ *     "path" = "",
+ *     "hide_blank_items" = TRUE,
+ *   },
+ *   instance_settings = {
+ *   },
+ *   default_widget = "field_collection_embed",
+ *   default_formatter = "field_collection_view"
+ * )
+ */
+class FieldCollection extends ConfigFieldItemBase {
+  /**
+   * {@inheritdoc}
+   */
+  public static function schema(FieldInterface $field) {
+    return array(
+      'columns' => array(
+        'value' => array(
+          'type' => 'int',
+          'not null' => TRUE
+        ),
+        'revision_id' => array(
+          'type' => 'int',
+          'not null' => FALSE
+        ),
+      ),
+    );
+  }
+}
