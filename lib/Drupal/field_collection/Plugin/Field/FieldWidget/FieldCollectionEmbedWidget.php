a537f1d072ac5cd9b7df37021f49bd02870a3170
jmuzz <joel.muzzerall@gmail.com>
2013-12-19 13:26:02 -0400

    Issue #1443960 by jmuzz: Added a stub for the embed widget and commented out some parts of field_collection.module causing errors

---
+++ lib/Drupal/field_collection/Plugin/Field/FieldWidget/FieldCollectionEmbedWidget.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\field_collection\Plugin\Field\FieldWidget\FieldCollectionEmbedWidget.
+ */
+
+namespace Drupal\field_collection\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\WidgetBase;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+
+/**
+ * Plugin implementation of the 'field_collection_embed' widget.
+ *
+ * @FieldWidget(
+ *   id = "field_collection_embed",
+ *   label = @Translation("Embedded"),
+ *   field_types = {
+ *     "field_collection"
+ *   },
+ *   settings = {
+ *   }
+ * )
+ */
+class FieldCollectionEmbedWidget extends WidgetBase {
+    /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, array &$form_state) {
+    return $element;
+  }
+}
